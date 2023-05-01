8dfae9b96174c19ed0159f9987fb2397b47894c4
Efener <118131646+Efener@users.noreply.github.com>
2023-05-01 14:06:59 +0300

    Add files via upload
    Question 4:
    Create a txt file with 10 positive integers of your choice written inside and place it in the
    root folder of your project.
    Within your cpp file, create a prototype function called swap_with_file that takes integer
    changes its value with a randomly selected value from your file. (You need to read the file
    contents)
    In the main function, create two integer variables x (define a value of your choice) and y
    and make y an empty reference. Then print out the original values of x and y. Then pass
    the y into your swap function. Then, send your y value into swap function as a parameter.
    At the end of your program, print out values of both x and y again.

---
+++ Question4.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <ctime>
+#include <string>
+
+void swap_with_file(int& num, std::string filename) {
+    std::ifstream file(filename);  //ifstream to open a file in read
+
+    int num_lines = 0;
+    std::string line;
+    while (std::getline(file, line)) {   //Getting the number of lines using getLine function via fstream library.
+        num_lines++;
+    }
+
+
+    file.close();
+    file.open(filename);
+    //close the file and open again.
+
+    int rand_line = std::rand() % num_lines;   // generate random line number by using srand
+    for (int i = 0; i < rand_line; ++i) {
+        std::getline(file, line);    // go to the selected random line and take the number inside of it.
+    }
+    num = std::stoi(line);  // converts the line(it's a string) to int.
+
+    // close file
+    file.close();
+}
+
+int main() {
+    std::srand(std::time(nullptr));
+
+    int x;
+    std::cout << "Please enter a integer value:\n";
+    std::cin >> x;
+
+    std::cout << "Original value: " << x << "\n";
+
+    swap_with_file(x, "numbers.txt");
+
+    std::cout << "New value: " << x << "\n";
+    std::cout << "Heh heh heh Efener was here";
+
+    return 0;
+}
+
